f6f4c5218ab76476304a81bcc38d692e57e2292c
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2025-04-05 23:28:53 +0200

    fix: reworked how fire is used to call the cli to avoid using the pager for the help page at the end for no reason
    Signed-off-by: thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>

--- save_to_zotero/__main__.py
+++ save_to_zotero/__main__.py
@@ -9,8 +9,16 @@ from .save_to_zotero import SaveToZotero
 
 def main():
     """Main CLI entry point."""
-    fire.Fire(SaveToZotero)
+    args = [arg.lower() for arg in sys.argv]
+    helps = ["h", "help", "-h", "--help"]
+    if any(h in args for h in helps):
+        # detected help page wanted, so calling the class directly
+        _ = fire.Fire(SaveToZotero)
+    else:
+        args, kwargs = fire.Fire((lambda *args, **kwargs: (args, kwargs)))
+        _ = SaveToZotero(*args, **kwargs)
+    return None
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    main()
